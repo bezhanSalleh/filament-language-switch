171a69e3bff8890fce2b63516d60fbeae7c14d10
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-11-25 08:17:30 +0430

    view cleaned

--- resources/views/language-switch.blade.php
+++ resources/views/language-switch.blade.php
@@ -1,77 +1,3 @@
-{{-- <div x-data="{
-    isLocaleDropdownOpen: false,
-    direction: @js(__('filament::layout.direction') ?? 'ltr'),
-    isRTL() {
-        if (this.direction === 'rtl') {
-            return true
-        }
-        return false
-    }
-}" class="relative"
-    :style="(isRTL() && { margin: '0 1rem 0 0' }) || (!isRTL() && { margin: '0 0 0 1rem' })">
-
-    <button x-on:click="isLocaleDropdownOpen = ! isLocaleDropdownOpen"
-        class="flex items-center justify-center w-10 h-10 border border-gray-300 rounded-full shrink-0 text-primary-500 filament-language-switch-button hover:bg-gray-500/5 focus:bg-primary-500/10 focus:outline-none lg:mr-2 rtl:lg:mr-0 rtl:lg:ml-2">
-        @if (config('filament-language-switch.flag'))
-            <x-dynamic-component :component="'flag-1x1-' .
-                (!blank(config('filament-language-switch.locales.' . app()->getLocale() . '.flag_code'))
-                    ? config('filament-language-switch.locales.' . app()->getLocale() . '.flag_code')
-                    : 'un')"
-                class="rounded-full w-9 h-9 group-hover:text-white group-focus:text-white text-primary-500" />
-        @else
-            <span
-                class="flex items-center justify-center font-semibold text-white rounded-full w-9 h-9 group-hover:text-white group-focus:text-white bg-primary-500">
-                {{ str(app()->getLocale())->length() > 2
-                    ? str(app()->getLocale())->substr(0, 2)->upper()
-                    : str(app()->getLocale())->upper() }}
-            </span>
-        @endif
-    </button>
-
-    <div x-show="isLocaleDropdownOpen" x-on:click.away="isLocaleDropdownOpen = false" x-transition:enter="transition"
-        x-transition:enter-start="-translate-y-1 opacity-0" x-transition:enter-end="translate-y-0 opacity-100"
-        x-transition:leave="transition" x-transition:leave-start="translate-y-0 opacity-100"
-        x-transition:leave-end="-translate-y-1 opacity-0"
-        class="absolute right-0 z-10 mt-2 shadow-xl rtl:right-auto rtl:left-0 rounded-xl w-52 top-full" x-cloak>
-        <ul @class([
-            'py-1 space-y-1 overflow-hidden bg-white shadow rounded-xl',
-            'dark:border-gray-600 dark:bg-gray-700' => config('filament.dark_mode'),
-        ])>
-            @foreach (config('filament-language-switch.locales') as $key => $locale)
-                <li @class([
-                    'flex items-center justify-between group px-2',
-                    'focus:outline-none hover:text-primary-600 focus:text-primary-600  cursor-pointer' => !app()->isLocale(
-                        $key
-                    ),
-                ])>
-                    <a wire:click="changeLocale('{{ $key }}')"
-                        class="flex items-center w-full h-8 px-2 text-sm font-medium whitespace-nowrap filament-dropdown-item">
-                        @if (config('filament-language-switch.flag'))
-                            <x-dynamic-component :component="'flag-1x1-' . (!blank($locale['flag_code']) ? $locale['flag_code'] : 'un')"
-                                class="flex-shrink-0 w-5 h-5 mr-2 -ml-1 rtl:ml-2 rtl:-mr-1 group-hover:text-white group-focus:text-white text-primary-500"
-                                style="border-radius: 0.25rem" />
-                        @else
-                            <span
-                                class="w-6 h-6 flex items-center justify-center mr-2 -ml-1 flex-shrink-0 rtl:ml-2 rtl:-mr-1 @if (!app()->isLocale($key)) group-hover:bg-white group-hover:text-primary-600 group-hover:border group-hover:border-primary-500/10 group-focus:text-white @endif bg-primary-500/10 text-primary-600 font-semibold rounded-full p-1 text-xs">
-                                {{ str($locale['name'])->snake()->upper()->explode('_')->map(function ($string) use ($locale) {
-                                        return str($locale['name'])->wordCount() > 1 ? str()->substr($string, 0, 1) : str()->substr($string, 0, 2);
-                                    })->take(2)->implode('') }}
-                            </span>
-                        @endif
-                        <span class="truncate">
-                            {{ str($locale[config('filament-language-switch.native') ? 'native' : 'name'])->headline() }}
-                        </span>
-
-                    </a>
-                    @if (app()->isLocale($key))
-                        <x-dynamic-component component="heroicon-s-check"
-                            class="flex-shrink-0 w-4 h-4 mr-2 rtl:ml-2 text-primary-500" />
-                    @endif
-                </li>
-            @endforeach
-        </ul>
-    </div>
-</div> --}}
 <x-filament::dropdown placement="bottom-end">
     <style>
         .filament-dropdown-list-item-label {
@@ -85,7 +11,7 @@
         'mr-4' => __('filament::layout.direction') === 'rtl',
     ])>
         <div
-            class="w-10 h-10 rounded-full bg-primary-500  dark:bg-gray-900 text-white font-semibold bg-cover bg-center flex items-center justify-center">
+            class="flex items-center justify-center w-10 h-10 font-semibold text-white bg-center bg-cover rounded-full bg-primary-500 dark:bg-gray-900">
             {{ str(app()->getLocale())->length() > 2
                 ? str(app()->getLocale())->substr(0, 2)->upper()
                 : str(app()->getLocale())->upper() }}
