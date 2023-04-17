b17a4a7579135bd4dc2e407ce12e38d5cce1f346
Bezhan Salleh <bezhan_salleh@yahoo.com>
2023-04-17 18:02:04 +0430

    fixes #20, #22 and #24

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -4,19 +4,19 @@ namespace BezhanSalleh\FilamentLanguageSwitch\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class SwitchLanguageLocale
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse) $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        $locale = session()->get('locale') ?? $request->get('locale') ?? config('app.locale', 'en');
+        $locale = session()->get('locale') ?? $request->get('locale') ?? $request->cookie('filament_language_switch_locale') ??  config('app.locale', 'en');
 
         if (array_key_exists($locale, config('filament-language-switch.locales'))) {
             app()->setLocale($locale);
