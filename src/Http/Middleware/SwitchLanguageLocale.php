f033c8a6daf91951d6882edb12738f1adc8134d5
Steve Fontaine <steve@designdesite.com>
2023-12-27 22:59:16 +0100

    Update SwitchLanguageLocale.php
    When the middleware is added to the whole app and not only to Filament panels, livewire triggers request with JsonResponse. This PR intends to make it valid.

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class SwitchLanguageLocale
 {
-    public function handle(Request $request, Closure $next): \Illuminate\Http\Response | \Illuminate\Http\RedirectResponse
+    public function handle(Request $request, Closure $next): \Illuminate\Http\Response | \Illuminate\Http\RedirectResponse | \Illuminate\Http\JsonResponse
     {
         $locale = session()->get('locale')
             ?? $request->get('locale')
