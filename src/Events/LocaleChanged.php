2aa1b359b5aa8159362850f44909b56260899d61
Bezhan Salleh <bezhan_salleh@yahoo.com>
2024-01-09 10:49:19 +0430

    done

--- src/Events/LocaleChanged.php
+++ src/Events/LocaleChanged.php
@@ -2,20 +2,10 @@
 
 namespace BezhanSalleh\FilamentLanguageSwitch\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 class LocaleChanged
 {
-    use Dispatchable;
-    use InteractsWithSockets;
-    use SerializesModels;
-
-    /**
-     * Create a new event instance.
-     */
-    public function __construct(public string $locale)
-    {
+    public function __construct(
+        public string $locale
+    ){
     }
 }
