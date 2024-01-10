9a8ecb22d42b36071966beb35da22ef2569abe00
alkoumi <komy66@gmail.com>
2024-01-10 18:40:17 +0300

    get the default locale from laravel config before browser

--- src/Http/Middleware/SwitchLanguageLocale.php
+++ src/Http/Middleware/SwitchLanguageLocale.php
@@ -1,40 +1,41 @@
 <?php
-
-namespace BezhanSalleh\FilamentLanguageSwitch\Http\Middleware;
-
-use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
-use Closure;
-use Illuminate\Http\Request;
-
-class SwitchLanguageLocale
-{
-    public function handle(Request $request, Closure $next): \Illuminate\Http\Response | \Illuminate\Http\RedirectResponse
-    {
-        $locale = session()->get('locale')
-            ?? $request->get('locale')
-            ?? $request->cookie('filament_language_switch_locale')
-            ?? $this->getBrowserLocale($request)
-            ?? config('app.locale', 'en');
-
-        if (in_array($locale, LanguageSwitch::make()->getLocales())) {
-            app()->setLocale($locale);
-        }
-
-        return $next($request);
-    }
-
-    private function getBrowserLocale(Request $request): ?string
-    {
-        $appLocales = LanguageSwitch::make()->getLocales();
-
-        $userLocales = preg_split('/[,;]/', $request->server('HTTP_ACCEPT_LANGUAGE'));
-
-        foreach ($userLocales as $locale) {
-            if (in_array($locale, $appLocales)) {
-                return $locale;
-            }
-        }
-
-        return null;
-    }
-}
+	
+	namespace BezhanSalleh\FilamentLanguageSwitch\Http\Middleware;
+	
+	use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+	use Closure;
+	use Illuminate\Http\RedirectResponse;
+	use Illuminate\Http\Request;
+	use Illuminate\Http\Response;
+	
+	class SwitchLanguageLocale
+	{
+		public function handle(Request $request , Closure $next): Response|RedirectResponse
+		{
+			$locale = session()->get('locale')
+				?? $request->get('locale')
+				?? $request->cookie('filament_language_switch_locale')
+				?? config('app.locale' , 'en')
+				?? $this->getBrowserLocale($request);
+			if (in_array($locale , LanguageSwitch::make()->getLocales())) {
+				app()->setLocale($locale);
+			}
+			
+			return $next($request);
+		}
+		
+		private function getBrowserLocale(Request $request): ?string
+		{
+			$appLocales = LanguageSwitch::make()->getLocales();
+			
+			$userLocales = preg_split('/[,;]/' , $request->server('HTTP_ACCEPT_LANGUAGE'));
+			
+			foreach ($userLocales as $locale) {
+				if (in_array($locale , $appLocales)) {
+					return $locale;
+				}
+			}
+			
+			return null;
+		}
+	}
